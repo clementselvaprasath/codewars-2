2018d078777476ed3e21714b6f402e2eddafc89b
Alejandro Aranda Gomez <alejandro.aranda.contractor@dev.bbva.com>
2017-10-28 21:37:03 +0200

    katas

--- src/main/java/moleculetoatoms/ParseMolecule.java
+++ src/main/java/moleculetoatoms/ParseMolecule.java
@@ -7,50 +7,45 @@ import java.util.HashMap;
 
 class ParseMolecule {
 
-    public static class Expression {
-        private String expression;
+    interface Expression {
+    }
+
+    public static class Group implements Expression {
+        private Character open;
+        private Character close;
+        private List<Expression> subexpressions;
         private int number;
 
-        public Expression(String expression, int number) {
-            this.expression = expression;
+        public Group(Character open, Character close, List<Expression> subexpressions, int number) {
+            this.open = open;
+            this.close = close;
+            this.subexpressions = subexpressions;
             this.number = number;
         }
 
         @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-
-            Expression that = (Expression) o;
-
-            if (number != that.number) return false;
-            return expression != null ? expression.equals(that.expression) : that.expression == null;
-        }
-
-        @Override
-        public int hashCode() {
-            int result = expression != null ? expression.hashCode() : 0;
-            result = 31 * result + number;
-            return result;
-        }
-
-        @Override
         public String toString() {
-            return "Expression{" +
-                    "expression='" + expression + '\'' +
-                    ", number=" + number +
-                    '}';
+            String exp = "" + (open != null ? open : "");
+            for (Expression e : subexpressions) {
+                exp += e.toString();
+            }
+            exp = exp + (close != null ? close : "") + (number == 1 ? "" : number);
+            return exp;
         }
     }
 
-
-    public static class Element {
+    public static class Element implements Expression {
         private String atom;
         private int number;
 
         public Element(String atom, int number) {
             this.atom = atom;
             this.number = number;
+        }
+
+        @Override
+        public String toString() {
+            return this.atom + (this.number == 1 ? "" : this.number);
         }
 
         @Override
@@ -70,79 +65,35 @@ class ParseMolecule {
             result = 31 * result + number;
             return result;
         }
-
-        @Override
-        public String toString() {
-            return "Element{" +
-                    "atom='" + atom + '\'' +
-                    ", number='" + number + '\'' +
-                    '}';
-        }
-
-        public String removeFrom(String formula) {
-            String toRemove = this.atom + (this.number == 1 ? "" : this.number);
-            return formula.replaceFirst(toRemove, "");
-        }
     }
 
     public static Map<String,Integer> getAtoms(String formula) {
+        System.out.println(formula);
         return read(formula, 1, new HashMap<>());
     }
 
     private static Map<String, Integer> read(String formula, int factor, Map<String, Integer> atoms) {
-        List<Element> elements = findElements(formula);
-        for (Element e : elements) {
-            addElement(factor, atoms, e);
+        List<Expression> expressions = findExpressions(formula);
+        for (Expression expression : expressions) {
+            readExpression(expression, factor, atoms);
         }
         return atoms;
     }
 
-    private static void addElement(int factor, Map<String, Integer> atoms, Element e) {
-        Integer number = atoms.get(e.atom);
-        if (number == null) {
-            number = 0;
+    public static List<Expression> findExpressions(String formula) {
+        List<Expression> expressions = new ArrayList<>();
+        Expression e;
+        while ((e = findExpression(formula)) != null) {
+            expressions.add(e);
+            formula = formula.substring(e.toString().length());
         }
-        atoms.put(e.atom, number + factor * e.number);
-    }
-
-    public static Element findElement(String formula) {
-        String element = "";
-        String number = "";
-        // First find element
-        int i = 0;
-        char c;
-        while (i < formula.length() && Character.isAlphabetic(c = formula.charAt(i))) {
-            element += c;
-            i++;
-        }
-        if (element.isEmpty()) {
-            return null;
-        }
-        // Then find element
-        while (i < formula.length() && Character.isDigit(c = formula.charAt(i))) {
-            number += c;
-            i++;
-        }
-        if (number.isEmpty()) {
-            number = "1";
-        }
-
-        return new Element(element, Integer.parseInt(number));
-    }
-
-    public static List<Element> findElements(String formula) {
-        List<Element> elements = new ArrayList<>();
-
-        Element e;
-        while ((e = findElement(formula)) != null) {
-            elements.add(e);
-            formula = e.removeFrom(formula);
-        }
-
-        return elements;
+        return expressions;
     }
 
     public static Expression findExpression(String formula) {
+        if (formula.isEmpty()) {
+            return null;
+        }
         char close = 0;
         char open = formula.charAt(0);
         if (open == '(') {
@@ -153,7 +104,7 @@ class ParseMolecule {
             close = ']';
         }
         if (close != 0) {
-            int closeIndex = formula.lastIndexOf(close);
+            int closeIndex = findClose(formula, open, close);
             if (closeIndex != -1) {
                 int i = closeIndex + 1;
                 String number = "";
@@ -165,10 +116,83 @@ class ParseMolecule {
                 if (number.isEmpty()) {
                     number = "1";
                 }
-                return new Expression(formula.substring(1, closeIndex), Integer.parseInt(number));
+                return new Group(open, close, findExpressions(formula.substring(1, closeIndex)), Integer.parseInt(number));
             }
+            throw new IllegalArgumentException("Wrong expression: " + formula);
+        }
+
+        if (Character.isAlphabetic(open) && Character.isUpperCase(open)) {
+            return findElement(formula);
+        }
+
+        throw new IllegalArgumentException("Wrong expression: " + formula);
+    }
+
+    public static int findClose(String formula, char open, char close) {
+        int inside = 0;
+        for (int i = 1; i < formula.length(); i++) {
+            char c = formula.charAt(i);
+            if (c == open) {
+                inside++;
+            } else if (c == close) {
+                if (inside > 0) {
+                    inside--;
+                } else {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static Element findElement(String formula) {
+        String element = "";
+        String number = "";
+        // First find element
+        int i = 0;
+        char c;
+        do {
+            c = formula.charAt(i);
+            if (Character.isAlphabetic(c)) {
+                // First uppercase, others lowercase
+                if ((i == 0 && Character.isUpperCase(c)) || (i > 0 && Character.isLowerCase(c))) {
+                    element += c;
+                } else {
+                    break;
+                }
+            } else if (Character.isDigit(c)) {
+                number += c;
+            } else {
+                break;
+            }
+            i++;
+        } while (i < formula.length());
+        if (element.isEmpty()) {
             return null;
         }
-        return new Expression(formula, 1);
+        if (number.isEmpty()) {
+            number = "1";
+        }
+
+        return new Element(element, Integer.parseInt(number));
+    }
+
+    private static void readExpression(Expression e, int factor, Map<String, Integer> atoms) {
+        if (e instanceof Element) {
+            addElement(factor, atoms, (Element)e);
+        } else {
+            Group g = (Group)e;
+            for (Expression s : g.subexpressions) {
+                readExpression(s, factor * g.number, atoms);
+            }
+        }
+    }
+
+    private static void addElement(int factor, Map<String, Integer> atoms, Element e) {
+        Integer number = atoms.get(e.atom);
+        if (number == null) {
+            number = 0;
+        }
+        atoms.put(e.atom, number + factor * e.number);
     }
 }
--- src/test/java/moleculetoatoms/ParserTest.java
+++ src/test/java/moleculetoatoms/ParserTest.java
@@ -2,38 +2,35 @@ package moleculetoatoms;
 
 import org.junit.Test;
 
-import java.util.List;
-
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class ParserTest {
 
     @Test
     public void testFindElement() {
+        assertEquals(null, ParseMolecule.findElement("pie"));
         assertEquals(null, ParseMolecule.findElement("2O"));
         assertEquals(new ParseMolecule.Element("O",1), ParseMolecule.findElement("O"));
         assertEquals(new ParseMolecule.Element("H", 2), ParseMolecule.findElement("H2O"));
+        assertEquals(new ParseMolecule.Element("O", 1), ParseMolecule.findElement("OH"));
+        assertEquals(new ParseMolecule.Element("Mg", 1), ParseMolecule.findElement("MgO"));
+        assertEquals(new ParseMolecule.Element("Mg", 1), ParseMolecule.findElement("Mg(OH)2"));
+        assertEquals(new ParseMolecule.Element("S", 1), ParseMolecule.findElement("SO3"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testException() {
+        ParseMolecule.getAtoms("pie");
     }
 
     @Test
-    public void testFindElements() {
-        assertEquals(0, ParseMolecule.findElements("2O").size());
-        assertTrue(ParseMolecule.findElements("O").contains(new ParseMolecule.Element("O",1)));
-
-        List<ParseMolecule.Element> h2O = ParseMolecule.findElements("H2O");
-        assertTrue(h2O.contains(new ParseMolecule.Element("H", 2)));
-        assertTrue(h2O.contains(new ParseMolecule.Element("O", 1)));
+    public void test1() {
+        ParseMolecule.getAtoms("{((H)2)[O]}");
     }
 
     @Test
-    public void findExpression() {
-        assertEquals(null, ParseMolecule.findExpression("(H2"));
-        assertEquals(new ParseMolecule.Expression("H2O", 1), ParseMolecule.findExpression("H2O"));
-        assertEquals(new ParseMolecule.Expression("H2O", 1), ParseMolecule.findExpression("(H2O)"));
-        assertEquals(new ParseMolecule.Expression("OH", 2), ParseMolecule.findExpression("(OH)2"));
-        assertEquals(new ParseMolecule.Expression("OH", 2), ParseMolecule.findExpression("{OH}2"));
-        assertEquals(new ParseMolecule.Expression("OH", 2), ParseMolecule.findExpression("[OH]2"));
+    public void test2() {
+        ParseMolecule.getAtoms("{[Co(NH3)4(OH)2]3Co}(SO4)3");
     }
 
 }
